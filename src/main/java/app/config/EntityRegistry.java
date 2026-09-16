b397ab74ce381b0b154ace49cb026f9108c2886e
jonasE-10 <loco.ellekjaer@gmail.com>
2026-09-16 12:08:26 +0200

    setup thing

---
+++ src/main/java/app/config/EntityRegistry.java
@@ -0,0 +1,12 @@
+package app.config;
+
+import org.hibernate.cfg.Configuration;
+
+final class EntityRegistry {
+
+    private EntityRegistry() {}
+
+    static void registerEntities(Configuration configuration) {
+        // TODO: Add more entities here...
+    }
+}
